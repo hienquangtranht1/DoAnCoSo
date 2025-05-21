492bfacc23441a8363c06155ca8176f1c6240611
Trần Quang Hiển <160805012+hienquangtranht1@users.noreply.github.com>
2025-05-21 12:47:49 +0700

    Delete config directory

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config/config.php
-
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");          // Với XAMPP, mật khẩu thường để trống
-define("DB_NAME", "datlichkhambenh");
-?>
